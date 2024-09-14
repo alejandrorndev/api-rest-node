847075f1b79c371185b23c76ecede6d29c8c5d15
Andres Gomez <andresgomez@MacBook-Pro-de-Andres.local>
2024-09-13 22:22:08 -0500

    Se crean endpoints para registrar usuarios y autenticar usuarios.

--- src/routes/UsuariosRoutes.js
+++ src/routes/UsuariosRoutes.js
@@ -6,4 +6,10 @@ const authToken = require("../utils/Autenticacion")
 
 router.get("/", authToken,  usersController.getAllUsers)
 
+router.post("/", usersController.registerUser)
+
+router.post("/auth/login", usersController.login)
+
+
+
 module.exports = router;
