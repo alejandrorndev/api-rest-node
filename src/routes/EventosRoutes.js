623f6b8b8504de1dc26f7ea5d5d70254e5a123b0
Andres Gomez <andresgomez@MacBook-Pro-de-Andres.local>
2024-09-14 12:47:10 -0500

    Se complementan los endpoints del servicio de eventos.

--- src/routes/EventosRoutes.js
+++ src/routes/EventosRoutes.js
@@ -2,16 +2,35 @@ const express = require("express");
 const router = express.Router();
 const eventsController = require('../controllers/EventoController')
 const authToken = require("../utils/Autenticacion")
-//const multer = require('multer');
+const multer = require('multer');
 
 
-//const storage = multer.memoryStorage();
-//const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.post("/", authToken, eventsController.createEvent)
 
 
 router.get("/", authToken, eventsController.getAllEvents)
+
+
+router.get("/:eventId", authToken, eventsController.getEvent)
+
+
+router.post("/LugaresCercanos", authToken, eventsController.getNearLocations)
+
+
+router.post("/LugaresCercanosAlEvento", authToken, eventsController.getNearLocationsFromEvent)
+
+
+router.post("/CargueMasivo", authToken, upload.single('file'),  eventsController.massiveCreationEvents)
+
+
+router.put("/:eventId", authToken, eventsController.updateEvent)
+
+
+router.delete("/:eventId", authToken, eventsController.deleteEvent);
+
 
 
 module.exports = router;
