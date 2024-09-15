e4763fa01fd84675404125dfdf835c3f99bf67d9
Andres Gomez <andresgomez@MacBook-Pro-de-Andres.local>
2024-09-15 18:00:13 -0500

    Se realiza correciones en las rutas de los usuarios para manejar de una forma mas limpia las validaciones.

--- src/routes/UsuariosRoutes.js
+++ src/routes/UsuariosRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const { validationResult } = require('express-validator');
 const router = express.Router();
 const usersController = require('../controllers/UsuarioController');
-const authToken = require("../utils/Autenticacion");
+const Autenticacion = require("../utils/Autenticacion");
 const {
     validarRegistroUsuario,
     validarInicioSesion,
@@ -11,51 +10,17 @@ const {
 } = require('../validaciones/validacionUsuarios');
 
 
-const validador = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-};
 
+router.post("/",validarRegistroUsuario,usersController.RegistrarUsuario);
 
-router.post("/",
-    validarRegistroUsuario,
-    validador, 
-    usersController.RegistrarUsuario
-);
+router.post("/inicio-sesion",validarInicioSesion,usersController.InicioDeSesion);
 
+router.get("/", Autenticacion, usersController.ObtenerUsuarios);
 
-router.post("/inicio-sesion",
-    validarInicioSesion,
-    validador, 
-    usersController.InicioDeSesion
-);
+router.get("/:usuarioId",validarUsuarioId,Autenticacion,usersController.ObtenerUsuario);
 
-router.get("/", authToken, usersController.ObtenerUsuarios);
+router.put("/:usuarioId",validarActualizacionUsuario,Autenticacion,usersController.ActualizarUsuario);
 
-
-router.get("/:usuarioId",
-    validarUsuarioId,
-    validador,
-    authToken,
-    usersController.ObtenerUsuario
-);
-
-
-router.put("/:usuarioId",
-    validarActualizacionUsuario,
-    validador,
-    authToken,
-    usersController.ActualizarUsuario
-);
-
-router.delete("/:usuarioId",
-    validarUsuarioId,
-    validador,
-    authToken,
-    usersController.EliminarUsuario
-);
+router.delete("/:usuarioId",validarUsuarioId,Autenticacion,usersController.EliminarUsuario);
 
 module.exports = router;
